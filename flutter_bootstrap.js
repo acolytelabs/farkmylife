e412a13d4a26a295b2abb42ef1af515c778b444a
Ralph <ralphhoyt@gmail.com>
2026-03-05 18:49:27 -0500

    fix tournament observe modal

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "734753700" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "622414117" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
