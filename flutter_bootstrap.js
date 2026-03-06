5a83bd0b784865863149f98294467a429160f1c5
Ralph <ralphhoyt@gmail.com>
2026-03-05 19:46:23 -0500

    explicit focus request all screens

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "674681930" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2461852336" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
