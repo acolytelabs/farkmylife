2f1fe9b5934380f814fa678d4ecada3b3e1b88d3
Ralph <ralphhoyt@gmail.com>
2026-03-05 19:24:54 -0500

    fix focus and tournament

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "600053666" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1283061042" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
