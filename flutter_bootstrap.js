3efafc07f370d83f6ab7bb5b44c0faa7cac46453
Ralph <ralphhoyt@gmail.com>
2026-03-06 17:57:45 -0500

    multiplayer firebase

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "402706820" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "873405840" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
