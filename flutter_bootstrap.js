956a3ac32fe1b494a994ced14d85bc68e97fa080
Ralph <ralphhoyt@gmail.com>
2026-03-06 20:20:30 -0500

    complete shop rebuild and vex building

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4095393724" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3233046305" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
